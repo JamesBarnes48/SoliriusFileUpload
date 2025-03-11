68f8016358ee8c2ec3a63b636151750eadfe8faf
James Barnes <jamesbarnes20082000@gmail.com>
2025-03-11 12:16:28 +0000

    comments structuring app.js and create fileUploadController signature

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+//npm packages
 const express = require('express');
 const multer = require('multer');
 
+//scripts
+const fileUpload = require('./fileUploadController.js');
+
+//setup express
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
+const port = 3000;
+app.listen(port, () => {console.log('app is listening on port ' + port)})
+
+//routes
 app.get('/', (req, res) => {
     res.send('hello');
 })
-
-const port = 3000;
-app.listen(port, () => {console.log('app is listening on port ' + port)})
