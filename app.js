c421ebeaf18195fda6ba2b675eeed7bc8f5550f6
James Barnes <jamesbarnes20082000@gmail.com>
2025-03-11 12:04:35 +0000

    setup nodemon

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
 const port = 3000;
 app.listen(port, () => {console.log('app is listening on port ' + port)})
