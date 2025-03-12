85da3a36bc5495d48f0d57c74c95591569a308f0
James Barnes <jamesbarnes20082000@gmail.com>
2025-03-12 14:39:21 +0000

    setup rate limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //npm packages
 const express = require('express');
 const multer = require('multer');
+const { rateLimit } = require("express-rate-limit");
 
 //scripts
 const fileUpload = require('./fileUploadController.js');
@@ -26,11 +27,20 @@ const upload = multer({
         cb(new Error('Invalid file type. Only CSV files are allowed.'));
     },
 });
+
+//setup rate limiter
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    limit: 10, // each IP can make up to 10 requests per minute
+    standardHeaders: 'draft-8', 
+    legacyHeaders: false,
+});
+
 //'name' attribute of the html input field which the csv file was uploaded into - found in req.body
 const fieldName = 'csvFile';
 
 //routes
-app.post('/upload', upload.single(fieldName), fileUpload.uploadFile);
+app.post('/upload', limiter, upload.single(fieldName), fileUpload.uploadFile);
 app.get('/validate', (req, res) => {
     setTimeout(() => {
         if(req.query.email?.includes('@')) return res.json({valid: true});
