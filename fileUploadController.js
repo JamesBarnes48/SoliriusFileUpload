4196aa55fcbd19c0aaac4970013f59458fdaca12
James Barnes <jamesbarnes20082000@gmail.com>
2025-03-14 00:41:29 +0000

    tracks upload stats using global map variable identified by uploadid

--- fileUploadController.js
+++ fileUploadController.js
@@ -2,10 +2,17 @@ const csv = require('csv-parser');
 const axios = require('axios');
 const stream = require('stream');
 const pLimit = require('p-limit');
+const crypto = require('crypto');
 
-exports.uploadFile = async (req, res) => {
+const currentlyUploading = new Map();
+
+exports.uploadFile = async (req, res) => {    
     //req.file is undefined when either not supplied or the file supplied is not csv
     if(!req.file) return res.status(400).send('No valid file received');
+
+    //generate upload id and allocate space for blank upload stats
+    const uploadID = req.body.uploadID || crypto.randomUUID();
+    currentlyUploading.set(uploadID, {totalRecords: 0, processedRecords: 0, failedRecords: 0, details: []});
 
     //configure readable stream
     const readableStream = new stream.Readable();
@@ -15,7 +22,7 @@ exports.uploadFile = async (req, res) => {
 
     const linePromises = [], limit = pLimit(5);
     readableStream.pipe(csv())
-        .on('data', (row) => {linePromises.push(limit(() => processLine(row)))})
+        .on('data', (row) => {linePromises.push(limit(() => processLine(row, uploadID)))})
         .on('end', async () => {
             //asynchronously validate csv rows
             let validationResults = [];
@@ -25,28 +32,29 @@ exports.uploadFile = async (req, res) => {
                 //could either handle errors line-by-line inside processLine or break out of Promise.all by handling them here
                 //figure that if an axios error occurs for one line then its likely to occur for all, therefore break out
                 res.status(500).send('Validation server error: ' + err.message);
+                currentlyUploading.delete(uploadID);
                 return;
             }
-
-            //initialise object to return and populate with validationResults
-            const recordStatistics = {totalRecords: 0, processedRecords: 0, failedRecords: 0, details: []}
-            validationResults.forEach((result) => {
-                recordStatistics.totalRecords++;
-                if(result.valid) recordStatistics.processedRecords++;
-                else{
-                    recordStatistics.failedRecords++;
-                    recordStatistics.details.push({...result.line, error: result.error});
-                }
-            })
-            res.json(recordStatistics);
+            //finalise upload stats and wipe from variable
+            const final = currentlyUploading.get(uploadID);
+            currentlyUploading.delete(uploadID);
+            res.json(final);
         });
 }
 
-const processLine = async (line) => {
-    //expect json response - {valid: boolean}
-    if(!line.email) return {line: line, valid: false, error: 'No email found'};
+const processLine = async (line, uploadID) => {
+    const uploadStats = currentlyUploading.get(uploadID);
+    if(!line.email){
+        uploadStats.failedRecords++;
+        uploadStats.details.push({...line, error: 'No email provided'});
+    }
     const isValid = await axios.get(`http://localhost:3000/validate`, {params: {email: line.email}, timeout: 5000});
-    return {line: line, valid: !!isValid.data?.valid, error: !isValid.data?.valid? 'Invalid email format': null};
+    uploadStats.totalRecords++;
+    if(!!isValid.data?.valid) uploadStats.processedRecords++;
+    else{
+        uploadStats.failedRecords++;
+        uploadStats.details.push({...line, error: 'Invalid email format'});
+    }
 }
 
 exports.checkStatus = (req, res) => {
